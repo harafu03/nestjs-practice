aaa7aa5818f7a3d885f1ec458967a5b5a7095932
KimHakSeong1203 <rmsp0030@gmail.com>
2025-02-13 10:02:47 +0900

    ctrl, module, service test

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+  boardService: BoardsService;
+  constructor(private boardsService: BoardsService) {
+    this.boardService = boardsService;
+  }
+
+  // @Get()
+  // getAllBoards() {
+  //   return this.boardService.getAllBoards();
+  // }
+}
