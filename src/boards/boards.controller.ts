de9dda8ef2a0d6bcf625d1b7e7fef4449e702f37
KimHakSeong1203 <rmsp0030@gmail.com>
2025-02-13 10:20:52 +0900

    add: board model

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
@@ -8,8 +9,8 @@ export class BoardsController {
     this.boardService = boardsService;
   }
 
-  @Get()
-  getAllBoards() {
+  @Get('/')
+  getAllBoards(): Board[] {
     return this.boardService.getAllBoards();
   }
 }
